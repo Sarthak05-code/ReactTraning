c181f70f1635b41c22e3da0fba2cddaffc6d86d9
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-25 16:20:59 +0545

    react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-
-import './App.css'
-
 function App() {
-  
+  const handleClick = () => {
+    alert("Hello , Sarthak");
+  };
 
   return (
-    <>
-      <h1 className='text-4xl font-bold bg-amber-400'>
-        If you read this your gay
-      </h1>
-    </>
-  )
+    <div className="flex justify-center items-center h-screen bg-slate-900">
+      {/* create a button here */}
+      <button
+        onClick={handleClick}
+        className="px-6 py-3 bg-amber-400 text-slate-950 font-bold rounded-lg hover:bg-amber-300 active:scale-95 transition-all"
+      >
+        Click Me
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
