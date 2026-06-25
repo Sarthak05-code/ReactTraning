2a86f704af3f352b286f263c493175b3c2353969
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-25 18:48:07 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ function App() {
     alert("Hello , Sarthak");
   };
 
+  const promptChecker = () => {
+    prompt("Hello , Sarthak. ")
+  }
+
   return (
     <div className="flex justify-center items-center h-screen bg-slate-900">
       {/* create a button here */}
@@ -12,6 +16,7 @@ function App() {
       >
         Click Me
       </button>
+      <button onClick={promptChecker} className="px-6 py-3 bg-sky-200 text-slate-950 font-bold rounded-lg gap-2 hover:bg-sky-300  active:scale-200 transition-all">Prompt</button>
     </div>
   );
 }
