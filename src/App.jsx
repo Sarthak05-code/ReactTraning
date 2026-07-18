64717e31f00bcecb6b8436fab348dd4bc824b900
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-18 12:52:26 +0545

    stuffs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 function App() {
+
+    
+  
   return (
     <>
+      <div className="mx-1 p-2 bg-amber-600 flex items-center justify-center text-lg font-extrabold text-gray-700">
         
+        
+        CleanState
+      </div>
+      <div className="mx-1 p-2 text-white font-bold cursor-pointer">
+        <li className="flex justify-around gap-1 bg-green-500">
+          <ul>Name</ul>
+          <ul>Class</ul>
+          <ul>About</ul>
+          <ul>Privacy</ul>
+        </li>
+        
+      </div>
     </>
   )
 }
